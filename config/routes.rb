267333f7af9979f717846565b9d751a511dbc1d1
Brian Han <brianhan87usa@gmail.com>
2014-01-27 16:44:11 -0500

    Add routes for UserSessions and login logout named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 IbmUnited::Application.routes.draw do
-  get "user_sessions/new"
-  get "user_sessions/create"
-  get "user_sessions/destroy"
-  resources :users
   root 'users#show'
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
