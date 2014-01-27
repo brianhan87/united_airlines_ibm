094559f749951580ab134d2d436733c9140f09f2
Brian Han <brianhan87usa@gmail.com>
2014-01-27 16:37:33 -0500

    Generate UserSessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 IbmUnited::Application.routes.draw do
+  get "user_sessions/new"
+  get "user_sessions/create"
+  get "user_sessions/destroy"
   resources :users
   root 'users#show'
   # The priority is based upon order of creation: first created -> highest priority.
