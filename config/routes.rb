90aaf692a9089b746b43e8b12527ceb5e6d6cf32
Brian Han <brianhan87usa@gmail.com>
2014-01-27 17:08:31 -0500

    Got rid of unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 IbmUnited::Application.routes.draw do
   root 'users#show'
-  resources :user_sessions
-  resources :users
+  resources :user_sessions, only: [:create, :destroy] 
+  resources :users, only: [:new, :create, :show]
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
