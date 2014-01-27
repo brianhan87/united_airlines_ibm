b70a3f25de111a136404608bc278668de5c8f068
Brian Han <brianhan87usa@gmail.com>
2014-01-27 15:34:15 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IbmUnited::Application.config.secret_key_base = 'ab995e6f755ac1e150787dc55af8445f10be2e92da01cdb365637eb8a333b629908b288da7a913abfa5c68e622553ef60d6249b44f8ece7f1535ada10f8ea459'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IbmUnited::Application.config.session_store :cookie_store, key: '_ibm_united_session'
