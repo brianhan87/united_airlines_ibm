c7c52b607039bd57c1aafe1ec471763feac398a6
Brian Han <brianhan87usa@gmail.com>
2014-01-27 15:42:32 -0500

    Add resources users for crud action routes and set root to user#show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 IbmUnited::Application.routes.draw do
+  resources :users
+  root 'user#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
