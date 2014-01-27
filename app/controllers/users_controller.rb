ac60da0f798f358589e4d34dfcd00c87ccd53249
Brian Han <brianhan87usa@gmail.com>
2014-01-27 16:58:13 -0500

    Add before filters to limit what users can do when logged in or out, and added first_name as a form field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_filter :require_login, only: [:index, :new, :create]
   def show
   end
 
@@ -7,6 +8,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to(@user, notice: 'User was successfully created')
+    else
+      render action: 'new'
+    end
   end
 
   def edit
@@ -21,6 +29,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :email, :password, :password_confirmation)
   end
 end
