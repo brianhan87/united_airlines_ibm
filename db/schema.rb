43401805bb2de6fd55d25c64cc2e4f09183d713d
Brian Han <brianhan87usa@gmail.com>
2014-01-29 12:44:43 -0500

    Add pg gem and configure db for postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140127205639) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: true do |t|
     t.string   "email",            null: false
     t.string   "crypted_password", null: false
@@ -23,6 +26,6 @@ ActiveRecord::Schema.define(version: 20140127205639) do
     t.string   "last_name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
